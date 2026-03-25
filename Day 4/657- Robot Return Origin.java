5e8dde814ec3b2b8c57921f397fd728eb04f1895
aishu553 <aishwarya89701@gmail.com>
2026-03-25 19:33:00 +0530

    Added Day 4 Solutions

---
+++ Day 4/657- Robot Return Origin.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x=0;
+        int y=0;
+        for(char c:moves.toCharArray()){
+            if(c=='L') x--;
+            else if(c=='R') x++;
+            else if(c=='U') y++;
+            else y--;
+        }
+        if(x==0 && y==0) return true;
+        return false;
+    }
+}
