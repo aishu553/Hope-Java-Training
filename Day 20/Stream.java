8b8e38b6ea34566bf6439e62adb1eaea989d4ddc
aishu553 <aishwarya89701@gmail.com>
2026-04-17 11:14:25 +0530

    Create Stream.java

---
+++ Day 20/Stream.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main{
+  public static void main(String[] args){
+    try{
+    //   Student s1=new Student("Janu",004);
+    //   FileOutputStream f=new FileOutputStream("test4.txt");
+    //   ObjectOutputStream obj=new ObjectOutputStream(f);
+    //   obj.writeObject(s1);
+    //   obj.close();
+
+    FileInputStream f=new FileInputStream("test4.txt");
+    ObjectInputStream obj=new ObjectInputStream(f);
+    Student s1=(Student) obj.readObject();
+    System.out.println(s.name+" "+s.rno+" "+s.age);
+    obj.close();
+    }
+
+  catch(IOException e){
+    
